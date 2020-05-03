f26473bd027fd787acae5fc683ab4110f39ad3f4
Clay Tercek <code@claytercek.com>
2020-05-03 16:57:09 -0400

    add sass

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
@@ -12,6 +13,16 @@ const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
+
+const preprocess = sveltePreprocess({
+  scss: {
+		includePaths: ['src'],
+    data: `@import './style/util.scss';` // have access to theme and mixins in every file
+  },
+  postcss: {
+    plugins: [require('autoprefixer')],
+  },
+});
 
 export default {
 	client: {
@@ -25,7 +36,8 @@ export default {
 			svelte({
 				dev,
 				hydratable: true,
-				emitCss: true
+				emitCss: true,
+        preprocess
 			}),
 			resolve({
 				browser: true,
@@ -34,7 +46,7 @@ export default {
 			commonjs(),
 
 			legacy && babel({
-				extensions: ['.js', '.mjs', '.html', '.svelte'],
+				extensions: ['.js', '.mjs', '.html', '.svelte', 'scss'],
 				runtimeHelpers: true,
 				exclude: ['node_modules/@babel/**'],
 				presets: [
@@ -68,7 +80,8 @@ export default {
 			}),
 			svelte({
 				generate: 'ssr',
-				dev
+				dev,
+        preprocess
 			}),
 			resolve({
 				dedupe: ['svelte']

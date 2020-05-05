a44c5d58a8026e65e27097d4a20d5e4c68eeeae9
Clay Tercek <code@claytercek.com>
2020-05-04 20:43:53 -0400

    fix heroku deploy

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,7 +1,5 @@
 // ensures that env variables are loaded before any other files
 import dotEnvExtended from 'dotenv-extended';
-export default dotEnvExtended.load({
-	errorOnMissing: true
-}); 
+export default dotEnvExtended.load(); 
 
  

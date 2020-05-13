b24aed01da76e15395054c4767d1285df937615c
Clay Tercek <code@claytercek.com>
2020-05-13 11:24:25 -0400

    mobile friendly layout

--- src/components/Data.svelte
+++ src/components/Data.svelte
@@ -5,20 +5,33 @@
 	import { colors } from "./stores";
 	import Legend from "./Legend.svelte";
 
+	let innerWidth, innerHeight;
+
   function grow(node, { delay = 0, duration = 500 }) {
-    const w = parseInt(getComputedStyle(node).width);
-    return {
-      delay,
-      duration,
-      css: t => {
-        const easedY = quartInOut(t);
-        const easedX = cubicIn(t);
-        return `
-          transform: scaleY(${easedY});
-          width: ${easedX * w};
-        `
-      }
-    };
+		const percent = node.dataset.percent
+		const w = percent * parseInt(getComputedStyle(node.parentElement).width);
+		const h = percent * parseInt(getComputedStyle(node.parentElement).height);
+
+		return {
+			delay,
+			duration,
+			css: t => {
+				const easedOffAxis = quartInOut(t);
+				const easedMainAxis = cubicIn(t);
+
+				if (innerWidth < innerHeight) { //portrait
+					return `
+						transform: scaleX(${easedOffAxis});
+						height: ${easedMainAxis * h};
+					`
+				} else { // landscape
+					return `
+						transform: scaleY(${easedOffAxis});
+						width: ${easedMainAxis * w};
+					`
+				}
+			}
+		};
 	}
 	
 	function shrink(node, { delay = 0, duration = 200 }) {
@@ -27,10 +40,12 @@
       delay,
       duration,
       css: (t) => {
-        const easedY = cubicIn(t);
-        return `
-          transform: scaleY(${easedY});
-        `
+				const easedOffAxis = cubicIn(t);
+				if (innerWidth < innerHeight) { //portrait
+					return `transform: scaleX(${easedOffAxis});`
+				} else { // landscape
+					return `transform: scaleY(${easedOffAxis});`
+				}
       }
     };
   }
@@ -43,7 +58,7 @@
 		height: 100%;
 		position: relative;
 	}
-	
+
 	.col-wrapper {
 		display: flex;
 		flex-direction: row;
@@ -51,8 +66,23 @@
 
 		list-style-type: none;
 		height: 100%;
+
+		@include portrait {
+			flex-direction: column;
+
+			li {
+				width: 100%;
+				height: auto;
+			}
+		}
+
+		li {
+			height: 100%;
+		}
 	}
 </style>
+
+<svelte:window bind:innerWidth={innerWidth} bind:innerHeight={innerHeight}/>
 
 <div>
 	<ul class="col-wrapper">
@@ -60,9 +90,11 @@
 			<li 
 				style={`
 					background-color:${$colors[col.val]};
-					width:${(col.tweets.length / $data.totalCount) * 100}%;`}
+					${innerWidth < innerHeight ? "height" : "width"}: ${(col.tweets.length / $data.totalCount) * 100}%;
+				`}
 				in:grow="{{delay: i * 50}}"
 				out:shrink="{{delay: i * 20}}"
+				data-percent={(col.tweets.length / $data.totalCount)}
 			/>
 		{/each}
 	</ul>
--- src/components/Legend.svelte
+++ src/components/Legend.svelte
@@ -5,25 +5,54 @@
 <style lang="scss">
 	.legend {
 		position: absolute;
-		left: 10vw;
+    left: 10vw;
+    right: 10vw;
 		bottom: calc(10vw - 30px);
-		width: calc(100% - 20vw);
 		background-color: white;
     height: 30px;
     color: $main-color;
 
+    @include portrait {
+      writing-mode: vertical-rl;
+      left: auto;
+      height: auto;
+      right: calc(10vw - 10px);
+      bottom: 10vw;
+      top: 10vw;
+      width: 30px;
+    }
+
 		p {
 			position: absolute;
 			bottom: 0;
-			left: 0;
+      left: 0;
+      
+      @include portrait {
+        left: auto;
+        bottom: auto;
+        top: 0;
+      }
 
 			&#positive {
 				right: 0;
-				left: auto;
+        left: auto;
+
+        @include portrait {
+          right: auto;
+          top: auto;
+          bottom: 0;
+        }
       }
       
       &#neutral {
         transform: translateX(-50%);
+        left: var(--offset);
+
+        @include portrait {
+          left: auto;
+          top: var(--offset);
+          transform: translateY(-50%);
+        }
       }
 		}
 	}
@@ -32,6 +61,6 @@
 
 <div class="legend">
 	<p id="negative">negative</p>
-	<p id="neutral" style={`left:${$percentNegative}%;`}>neutral</p>
+	<p id="neutral" style="--offset: {$percentNegative}%">neutral</p>
 	<p id="positive">positive</p>
 </div>

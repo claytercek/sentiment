b71d5e3f73253399d1eb2ab2cfc8652750cdbb8a
Clay Tercek <code@claytercek.com>
2020-05-14 11:54:07 -0400

    add SEO

---
+++ src/components/SEO.svelte
@@ -0,0 +1,32 @@
+<script>
+  let title = "sympathize"
+  let description = "Visualize current public sentiment of any topic by analyzing recent tweets."
+  let url = "https://sympathize.herokuapp.com"
+  let image = url + "/card.png"
+  let keywords = [
+    "twitter",
+    "sentiment",
+    "analysis",
+    "datavis"
+  ]
+
+</script>
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content="{description}" />
+  <meta name="keywords" content="{keywords.join(", ")}"/>
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="{url}">
+  <meta property="og:title" content="{title}">
+  <meta property="og:description" content="{description}">
+  <meta property="og:image" content="{image}">
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="{url}">
+  <meta property="twitter:title" content="{title}">
+  <meta property="twitter:description" content="{description}">
+  <meta property="twitter:image" content="{image}">
+</svelte:head>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,10 +1,12 @@
 <script>
-	// export let segment;
+	import SEO from "../components/SEO.svelte";
 </script>
 
 <style lang="scss" global>
 	@import "../style/global.scss";
 </style>
+
+<SEO />
 
 <main>
 	<slot></slot>
